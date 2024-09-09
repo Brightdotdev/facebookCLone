8adf2b9b61eb4bc152ccb0653acca7fd71ac394e
Bright <iamtherealbright@gmail.com>
2024-09-09 01:07:25 +0100

    Done with pushing some codes...

--- js/groups.js
+++ js/groups.js
@@ -2,6 +2,25 @@ import { nav } from "./navs.js";
 import { loadTheme } from "./miniFunctionlities.js";
 
 
-document.body.appendChild(nav("groups"))
 
-loadTheme()
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+
+    document.body.appendChild(nav("groups"))
+
+    loadTheme()
+    
+    document.querySelectorAll(".promise").forEach(promise => {
+        promise.addEventListener("click", () =>{
+            window.location.href = "../html/discoverGroups.html"
+        })
+    })
+      
+
+
+document.querySelector('.left-header').addEventListener('click', () =>{
+    window.location.href = './groups.html'
+
+})
+})
